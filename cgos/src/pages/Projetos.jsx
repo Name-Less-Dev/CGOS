39ed1330bf50ffe32a184c4f23777133034a2eaf
Name-Less-Dev <136519686+Name-Less-Dev@users.noreply.github.com>
2025-06-18 09:26:38 -0300

    Update Projetos.jsx

--- cgos/src/pages/Projetos.jsx
+++ cgos/src/pages/Projetos.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import { useEffect, useState } from "react";
 import { criarProjeto, listarProjetos, apagarProjeto } from "../services/ProjetoService";
 import { useNavigate } from "react-router-dom";
